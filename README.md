442c26c0d6e727d1c20a537e7d6f651809a46145
David Seapy <dseapy@gmail.com>
2022-12-15 09:30:38 -0500

    feat: initial setup
    Signed-off-by: David Seapy <dseapy@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+![Ksflow](images/ksflow-logo-3800x670-transparent.png)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+Ksflow is a lightweight tool to simplify processing Kafka topics from Kubernetes.
+
+## Features
+
+:heavy_check_mark: developer-friendly: language & client agnostic
+:heavy_check_mark: secure: mTLS & Kafka ACLs
+:heavy_check_mark: inexpensive: no required sidecars or daemonsets
+
+## Documentation
+- [Quick Start](./docs/quick-start.md)
---
+++ api/v1alpha1/ksflowconfig_types.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2022 The Ksflow Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	crv1 "sigs.k8s.io/controller-runtime/pkg/config/v1alpha1"
+)
+
+type KafkaTLSConfig struct {
+	// ClientCertificate is the path to the file holding the client-side TLS certificate to use
+	ClientCertificate string `json:"clientCertificate"`
+
+	// PrivateKey is the path to the file holding the client’s private key
+	PrivateKey string `json:"privateKey"`
+
+	// CACertificates is the path to the file containing certificate authority certificates to use
+	// in verifying a presented server certificate
+	CACertificates string `json:"caCertificates"`
+}
+
+type KafkaConfig struct {
+	// BootstrapServers provides the list of initial Kafka brokers to connect to
+	BootstrapServers []string `json:"bootstrapServers"`
+
+	// KafkaTLSConfig provides certificates for connecting to Kafka over mutual TLS
+	KafkaTLSConfig KafkaTLSConfig `json:"tls"`
+}
+
+//+kubebuilder:object:root=true
+
+// KsflowConfig is the configuration to run the controller manager, typically mounted in from ConfigMap
+type KsflowConfig struct {
+	metav1.TypeMeta `json:",inline"`
+
+	crv1.ControllerManagerConfigurationSpec `json:",inline"`
+
+	KafkaConfig KafkaConfig `json:"kafka"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KsflowConfig{})
+}
